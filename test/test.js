a1a51479ca541671bfa2030d3c0edad34b324023
Soichiro-Yoshimura <yoshimura@soichiro>
2016-03-16 20:03:21 +0900

    fix typo

--- test/test.js
+++ test/test.js
@@ -72,7 +72,7 @@ describe('/schedules', () => {
             .expect(/テスト候補2/)
             .expect(/テスト候補3/)
             .expect(200)
-            .end(() => { deleteScheduleAggrigate(createdSchedulePath.split('/schedules/')[1], done);});
+            .end(() => { deleteScheduleAggregate(createdSchedulePath.split('/schedules/')[1], done);});
         });
     });
   });
@@ -105,14 +105,14 @@ describe('/schedules/:scheduleId/users/:userId/candidates/:candidateId', () => {
               .post(`/schedules/${scheduleId}/users/${0}/candidates/${candidate.candidateId}`)
               .send({ availability: 2 }) // 出席に更新
               .expect('availability:2')
-              .end(() => { deleteScheduleAggrigate(scheduleId, done); });
+              .end(() => { deleteScheduleAggregate(scheduleId, done); });
           });
         });
     });
   });
 });
 
-function deleteScheduleAggrigate(scheduleId, done) {
+function deleteScheduleAggregate(scheduleId, done) {
   Availability.findAll({
     where: { scheduleId: scheduleId }
   }).then((availabilities) => {
